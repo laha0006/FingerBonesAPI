225ddb55dc096e75b0a4143c7137eadf462663c1
Lars <144362461+laha0006@users.noreply.github.com>
2025-02-20 23:45:57 +0100

    updated fingerbones api

--- app.js
+++ app.js
@@ -1,43 +1,92 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+
 const fingerBones = [
-  {
-    id: 1,
-    name: "Distal Phalanges",
-    description: "The tips of the fingers, used for gripping and sensing."
-  },
-  {
-    id: 2,
-    name: "Intermediate Phalanges",
-    description: "The middle bones in fingers, absent in the thumb."
-  },
-  {
-    id: 3,
-    name: "Proximal Phalanges",
-    description: "The bones closest to the hand, connecting fingers to the palm."
-  },
-  {
-    id: 4,
-    name: "Metacarpals",
-    description: "Bones in the palm that support finger movement."
-  },
+    {
+        id: 1,
+        name: "Distal Phalanges",
+        description: "The tips of the fingers, used for gripping and sensing."
+    },
+    {
+        id: 2,
+        name: "Intermediate Phalanges",
+        description: "The middle bones in fingers, absent in the thumb."
+    },
+    {
+        id: 3,
+        name: "Proximal Phalanges",
+        description: "The bones closest to the hand, connecting fingers to the palm."
+    },
+    {
+        id: 4,
+        name: "Metacarpals",
+        description: "Bones in the palm that support finger movement."
+    },
 ];
+let nextId = 5;
+
 
 const getFingerBoneById = (id) => {
-  return fingerBones.find(finger => finger.id === Number(id));
+    return fingerBones.find(finger => finger.id === Number(id));
 }
 
 app.get('/fingerbones', (req, res) => {
-  res.send(fingerBones);
+    res.send(fingerBones);
 })
 
 app.get('/fingerbones/:id', (req, res) => {
-  const fingerBone = getFingerBoneById(req.params.id)
-  if (!fingerBone) {
-     res.status(404).send({ error: "Resource not found" });
-  }
-  res.send(fingerBone)
+    const fingerBone = getFingerBoneById(req.params.id)
+    if (!fingerBone) {
+        res.status(404).send({error: "Resource not found"});
+    }
+    res.send(fingerBone)
+})
+
+app.post('/fingerbones', (req, res) => {
+    const newBone = req.body;
+    newBone.id = nextId++;
+    fingerBones.push(newBone);
+    res.send(newBone);
+})
+
+app.put('/fingerbones/:id', (req, res) => {
+    const fingerBone = getFingerBoneById(req.params.id)
+    if (!fingerBone) {
+        res.status(404).send({error: "Resource not found"});
+    }
+    const newBone = req.body;
+    fingerBone.name = newBone.name;
+    fingerBone.description = newBone.description;
+    res.send(fingerBone)
+})
+
+app.patch('/fingerbones/:id', (req, res) => {
+    const fingerBone = getFingerBoneById(req.params.id)
+    if (!fingerBone) {
+        res.status(404).send({error: "Resource not found"});
+    }
+    const newBone = req.body;
+    if (newBone.name) {
+        fingerBone.name = newBone.name;
+    }
+    if (newBone.description) {
+        fingerBone.description = newBone.description;
+    }
+    res.send(fingerBone)
+})
+
+app.delete('/fingerbones/:id', (req, res) => {
+    const fingerBone = getFingerBoneById(req.params.id)
+    if (!fingerBone) {
+        res.status(404).send({error: "Resource not found"});
+    }
+    const index = fingerBones.findIndex(bone => bone.id === +req.params.id);
+    console.log("index:",index)
+    fingerBones.splice(index, 1);
+    res.send(fingerBone)
 })
 
 app.listen(8080)
